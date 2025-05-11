a01c538e10e7c912e33ea2833336c5d17800ef6f
Santanu Rajak Das <santanu.das6104@gmail.com>
2025-05-11 12:41:30 +0530

    Added a clickable feature

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,29 +4,31 @@ import no_movie from '../assets/no-movie.png'
 
 const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language, id } }) => {
     return (
-        <div className='movie-card cursor-pointer'>
-            <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : `${no_movie}`} alt={title} />
-            <div className='mt-4'>
-                <h3 className='text-xl'>{title}</h3>
-            </div>
-
-            <div className='content'>
-                <div className='rating'>
-                    <img src={star} alt="star" />
-                    <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
-
-                    <span>•</span>
-                    <p className='lang'>{original_language.toUpperCase()}</p>
-
-                    <span>•</span>
-                    <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+        <a href={`https://www.google.com/search?q=${title}+movie`} blank target='_blank'>
+            <div className='movie-card cursor-pointer'>
+                <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : `${no_movie}`} alt={title} />
+                <div className='mt-4'>
+                    <h3 className='text-xl'>{title}</h3>
                 </div>
-                <div className='date'>
-                    
-                    
+
+                <div className='content'>
+                    <div className='rating'>
+                        <img src={star} alt="star" />
+                        <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+
+                        <span>•</span>
+                        <p className='lang'>{original_language.toUpperCase()}</p>
+
+                        <span>•</span>
+                        <p className='year'>{release_date ? release_date.split('-')[0] : 'N/A'}</p>
+                    </div>
+                    <div className='date'>
+
+
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     )
 }
 
